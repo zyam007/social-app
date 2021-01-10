f2fe03eee86015ca7ec83a07f56ba1cfe80d06b6
Yanna Sk <zyam007@gmail.com>
2021-01-10 03:11:20 -0500

    basic start

---
+++ src/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Header.css';
+import SearchIcon from '@material-ui/icons/Search';
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <img
+          src="https://www.flaticon.com/svg/static/icons/svg/174/174857.svg"
+          alt=""
+        />
+
+        <div className="header__search">
+          <SearchIcon />
+          <input type="text" />
+        </div>
+      </div>
+
+      <div className="header__right"></div>
+    </div>
+  );
+}
+
+export default Header;
